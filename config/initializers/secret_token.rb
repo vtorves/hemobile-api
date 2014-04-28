7bd10664e9621126e06e1eeafc4eebec571a2c3a
Vinicius Torves <vinicius@torves.com>
2014-04-28 16:04:48 -0300

    Initial commit.
    Generated by pah version 0.0.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HemobileApi::Application.config.secret_key_base = 'fcf18b4c60bdccfb010dec0b7423f4fd8ac10fa124d88393049171b9772390f0af595ccddd9ea1a88e89d0873823581545adb086c0d3021e07d243bcde7a2e8e'
