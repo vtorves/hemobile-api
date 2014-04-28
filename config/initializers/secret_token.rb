d67a1bfb8cbaf14d8b50def32a770997080c68ca
Vinicius Torves <vinicius@torves.com>
2014-04-28 16:06:41 -0300

    Replace secret token with environment variable.
    Generated by pah version 0.0.13

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-HemobileApi::Application.config.secret_key_base = 'fcf18b4c60bdccfb010dec0b7423f4fd8ac10fa124d88393049171b9772390f0af595ccddd9ea1a88e89d0873823581545adb086c0d3021e07d243bcde7a2e8e'
+HemobileApi::Application.config.secret_key_base = ENV['SECRET_TOKEN']
